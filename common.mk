509ce1ebb854af49301993604291deb896ca0259
Envoy-Z-Lab <envoyzlab@gmail.com>
2024-02-15 21:28:44 +0530

    sdm845-common: Bringup for @GenesisOS

--- common.mk
+++ common.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-genesis
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
